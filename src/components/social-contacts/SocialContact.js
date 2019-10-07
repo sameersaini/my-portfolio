e88980e0d664450369d7dbb6aaa70d494b20b487
sameer <sameersaini40@gmail.com>
2019-10-06 18:53:05 -0600

    adds nice css

--- src/components/social-contacts/SocialContact.js
+++ src/components/social-contacts/SocialContact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function SocialContact(props) {
     return (
-        <div>
+        <div className="social-icon">
             <a href={props.url} className="text-white" target="_blank" rel="noopener noreferrer">
                 <i className={`fab fa-${props.icon} icon-size`} />
             </a>
